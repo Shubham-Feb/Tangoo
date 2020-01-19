210e9ce12dafc1dfd612466948558e7d3dbbd23f
Shubham-Feb <59606035+Shubham-Feb@users.noreply.github.com>
2020-01-20 00:48:04 +0530

    Update Sample1.java

--- src/org/git/Sample1.java
+++ src/org/git/Sample1.java
@@ -5,10 +5,8 @@ public class Sample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Shubham_Singh");
-		System.out.println("Shubham_Srivastav");
-		System.out.println("Ankit_Upadhayay");
-		System.out.println("Mudit_pratap_Singh");
+		System.out.println("Singh");
+		
 
 	}
 
